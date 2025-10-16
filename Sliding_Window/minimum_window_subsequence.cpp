8533b33ea153d03d7020cdc7df67910a8bc4da1e
Anish17Bhat <bhatanish17@gmail.com>
2025-10-16 20:27:34 +0530

    cleared the mess and solved it

--- Sliding_Window/minimum_window_subsequence.cpp
+++ Sliding_Window/minimum_window_subsequence.cpp
@@ -5,53 +5,41 @@ using namespace std;
 class Solution
 {
 public:
-	string func(string s, string t, int n, int m, string str, int ind, string ans, int minL)
-	{
-		if (ind == n)
-		{
-			cout << str << endl;
-			string temp = "";
-			for (int i = 0; i < str.size(); i++)
-			{
-				if (t.find(str[i]) != string::npos)
-				{
-					temp += str[i];
-				}
-				if (temp == t)
-				{
-					if (str.size() < minL)
-					{
-						minL = str.size();
-						ans = str;
-					}
-					break;
-				}
-			}
-			return ans;
-		}
-		if (ind == 0)
-			cout << "heloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" << endl;
-		str.push_back(s[ind]);
-		if (ind == 0)
-			cout << "hello" << endl;
-		if (ind == 1)
-			cout << "hello" << endl;
-		ans = func(s, t, n, m, str, ind + 1, ans, minL);
-		str.pop_back();
-		ans = func(s, t, n, m, str, ind + 1, ans, minL);
-		return ans;
-	}
 	string minWindow(string s, string t)
 	{
 		int m = t.size();
 		int n = s.size();
-		return func(s, t, n, m, "", 0, "", INT_MAX);
+		int j = 0;
+		int minL = INT_MAX;
+		string minWindowString = "";
+		for(int i = 0 ; i < n ; i++){
+			if(s[i] == t[j]){
+				j++;
+			}
+			if(j == m){
+				int end = i;
+				j--;
+				while(j >= 0){
+					if(s[i] == t[j]){
+						j--;
+					}
+					i--;
+				}
+				i++;
+				j++;
+				if(end - i + 1 < minL){
+					minL = end - i + 1;
+					minWindowString = s.substr(i,minL);
+				}
+			}
+		}
+		return minWindowString;
 	}
 };
 
 int main()
 {
-	string s = "ADBCO";
+	string s = "ADOBECODEBANC";
 	string t = "ABC";
 	Solution s1;
 	cout << endl
