17ed015936a597eec6d924720f0c1de85a48096d
Anish17Bhat <bhatanish17@gmail.com>
2025-10-22 16:44:24 +0530

    update

--- Heap/KthLargestusingHeap.cpp
+++ Heap/KthLargestusingHeap.cpp
@@ -7,34 +7,37 @@ class Solution {
         int max = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
-        if(left < n && nums[left] < nums[max]) max = left;
-        if(right < n && nums[right] < nums[max]) max = right;
+        if(left < n && nums[left] > nums[max]) max = left;
+        if(right < n && nums[right] > nums[max]) max = right;
         if(max != i){
             swap(nums[max],nums[i]);
             heapify(nums,n,max);
         }
     }
-    void heap_sort(vector<int>&nums, int n){
-        for(int i = (n/2)-1 ; i >= 0 ; i--){
-            heapify(nums,n,i);
-        }
-        for(int i = n - 1 ; i >= 0 ; i--){
-            swap(nums[0],nums[i]);
-            heapify(nums,i,0);
-        }
+    void constructHeap(vector<int>&nums,int k){
+        for(int i = (k/2)-1 ; i >= 0  ; i--)
+            heapify(nums,k,i);
     }
 public:
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size();
-        heap_sort(nums,n);
-        return nums[k-1];
+        constructHeap(nums,k);
+        for(int i = k ; i < n ; i++){
+            if(nums[i] < nums[0]){
+                swap(nums[0],nums[k-1]);
+                nums[k-1] = nums[i];
+                constructHeap(nums,k);
+            }
+        }
+        return nums[0];
     }
 };
 
 int main(){
     Solution s;
-    vector<int>nums = {3,2,1,5,6,4};
-    cout<<endl<<s.findKthLargest(nums,2)<<endl;
+    vector<int>nums = {7,10,4,20,15};
+    cout<<endl<<s.findKthLargest(nums,4)<<endl;
+    cout<<endl<<endl;
     for(int ele : nums){
         cout<<ele<<" ";
     }
