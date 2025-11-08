87ac7b9c24973ffcc63c7a84c9cde99c95c87171
Anish17Bhat <bhatanish17@gmail.com>
2025-11-08 20:12:37 +0530

    update

--- Arrays/128_LongesConsSeq.cpp
+++ Arrays/128_LongesConsSeq.cpp
@@ -6,18 +6,22 @@ using namespace std;
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int maxLen = 0;
         int len = 1;
-        if(nums.size() == 1) return len;
-        for(int i = 1 ; i < nums.size() ; i++){
-            if(nums[i] - nums[i-1] == 1) len++;
-            else if(nums[i] == nums[i-1]){
+        set<int>s;
+        for(int ele : nums){
+            s.insert(ele);
+        }
+        for(int ele : s){
+            if(s.find(ele-1) == s.end()){
+                len = 1;
+                int temp = ele+1;
+                while(s.find(temp) != s.end()){
+                    len++;
+                    temp++;
+                }
                 maxLen = max(len,maxLen);
-                continue;
-            } 
-            else len = 1;
-            maxLen = max(len,maxLen);
+            }
         }
         return maxLen;
     }
