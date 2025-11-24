a517c1aa9385fccef84c3a1b584f58a01fc58eae
Anish17Bhat <bhatanish17@gmail.com>
2025-11-24 20:44:37 +0530

    update

---
+++ Recursion/39_Combination_sum.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    bool helper(int i , int sum , vector<int>&temp, vector<vector<int>>&result, vector<int>&candidates, int target,int n){
+        if(sum == target) {
+            result.push_back(temp);
+            return true;
+        }
+        else if(sum > target) return false;
+
+        for(int j = i ; j < n ; j++){
+            sum += candidates[j];
+            temp.push_back(candidates[j]);
+            if(helper(j,sum,temp,result,candidates,target,n) == false){
+                sum -= candidates[j];
+                temp.pop_back();
+                break;
+            };
+            sum -= candidates[j];
+            temp.pop_back();
+        }
+        return true;
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size();
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>result;
+        vector<int>temp;
+        int sum = 0;
+        helper(0,sum,temp,result,candidates,target,n);      
+        return result;
+    }
+};
+
+int main(){
+    vector<int>candidates = {1,4,2,6};
+    int target = 6;
+    Solution s;
+    vector<vector<int>> result = s.combinationSum(candidates,target);
+    for(vector<int> ele : result){
+        for(int e : ele){
+            cout<<e<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
