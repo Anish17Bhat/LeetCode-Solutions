2dca92f23119e21d9b702970728930a5228b10a6
Anish17Bhat <bhatanish17@gmail.com>
2025-12-03 17:36:02 +0530

    update

--- Greedy/N_meetings_in_room.cpp
+++ Greedy/N_meetings_in_room.cpp
@@ -6,38 +6,32 @@ class  Solution{
     public:
         vector<int> MaxNumMeetings(vector<int>&start,vector<int>&end,int n){
             map<pair<int,int>,int>meetings;
-            vector<int>result;
             for(int i = 0 ; i < n ; i++){
-                meetings.insert({make_pair(start[i],end[i]),i+1});
+                meetings.insert({make_pair(end[i],start[i]),i+1});
             }
             cout<<endl;
             for(auto meet : meetings){
                 cout<<"( "<<meet.first.first<<","<<meet.first.second<<" )"<<"  "<<meet.second<<endl;
             }
             cout<<endl;
+            vector<int>ans;
+            auto t = *(meetings.begin());
+            pair<int,int>temp = t.first;
+            ans.push_back(t.second);
             for(auto meet : meetings){
-                pair<int,int>duration = meet.first;
-                if(duration.first == duration.second) continue;
-                vector<int>ans;
-                ans.push_back(meet.second);
-                for(auto nxtmeet : meetings){
-                    if(nxtmeet.first.first >= duration.second){
-                        ans.push_back(nxtmeet.second);
-                        duration = nxtmeet.first;
-                    }
-                }
-                if(ans.size() > result.size()){
-                    result = ans;
+                if(meet.first.second >= temp.first){
+                    ans.push_back(meet.second);
+                    temp = meet.first;
                 }
             }
-            return result;
+            return ans;
         }
     };
     
     int main(){
         Solution s;
         vector<int>start = {1,3,0,5,8,5};
-        vector<int>end = {1,4,5,9,9,7};
+        vector<int>end = {5,4,5,9,9,7};
         int n = 6;
         vector<int>result = s.MaxNumMeetings(start,end,n);
         for(int ele : result){
