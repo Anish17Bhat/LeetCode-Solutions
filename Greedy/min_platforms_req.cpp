d39df9bd9359d170c6595dfdaade7f1fcac0dd94
Anish17Bhat <bhatanish17@gmail.com>
2025-12-05 23:12:16 +0530

    update

--- Greedy/min_platforms_req.cpp
+++ Greedy/min_platforms_req.cpp
@@ -23,28 +23,36 @@ class Solution{
             }          
             sort(tm,tm+n,comp);
             int count = 1;
-            int maxCount = 1;
-            int prevDep = tm[0].dt;
+            int platforms = 1;
+            struct timings p[n];
+            p[0].at = tm[0].at;
+            p[0].dt = tm[0].dt;
             for(int i = 1 ; i < n ; i++){
-                if(tm[i].at < prevDep){
-                    count++;
-                    prevDep = tm[i].dt;
+                int free = -1;
+                for(int j = 0 ; j < platforms ; j++){
+                    if(p[j].dt < tm[i].at){
+                        free = j;
+                        break;
+                    }
                 }
+                if(free == -1){
+                    platforms++;
+                    p[platforms-1].at = tm[i].at;
+                    p[platforms-1].dt = tm[i].dt;
+                } 
                 else{
-                    count = 1;
-                    prevDep = tm[i].dt;
+                    p[free].dt = tm[i].dt;
                 }
-                maxCount = max(count,maxCount);
             }
-            return maxCount;
+            return platforms;
         }
 };
 
 int main(){
     Solution s;
-    vector<int>arr = {1020,1200,1200}; 
+    vector<int>arr = {900,945,955,1100,1500,1800}; 
     // {900,945,955,1100,1500,1800};
-    vector<int>dep = {1050,1230,1220};
+    vector<int>dep = {920,1200,1130,1150,1900,2000};
     // {920,1200,1130,1150,1900,2000};
     cout<<s.minPlatforms(arr,dep,6);
     cout<<endl;
