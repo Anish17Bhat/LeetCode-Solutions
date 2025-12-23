ea871dbb71f6c11348f93f0c33fe166d533c5b53
Anish17Bhat <bhatanish17@gmail.com>
2025-12-23 13:22:33 +0530

    update

---
+++ Greedy/435_non_overlapping.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        int i = 0;
+        sort(intervals.begin(),intervals.end());
+        int ans = 0;
+        vector<int>tempInterval = intervals[0];
+        for(int i = 1 ; i < n ; i++){
+            if(intervals[i][0] < tempInterval[1]){
+                ans++;
+                tempInterval[1] = min(intervals[i][1],tempInterval[1]);
+            }
+            else{
+                tempInterval = intervals[i];
+            }
+        }
+        return ans;     
+    }
+};
+
+int main(){
+    Solution s;
+    vector<vector<int>>intervals = {{1,3},{2,6},{8,10},{15,18},{17,20}};
+    int result = s.merge(intervals);
+    cout<<result<<endl;
+    return 0;
+}
