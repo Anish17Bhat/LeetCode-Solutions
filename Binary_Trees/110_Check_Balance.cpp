1c149fd84819abd358bbdb9db9d3c897ca7287e5
Anish17Bhat <bhatanish17@gmail.com>
2026-01-01 16:13:45 +0530

    update

--- Binary_Trees/110_Check_Balance.cpp
+++ Binary_Trees/110_Check_Balance.cpp
@@ -10,29 +10,39 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
 class Solution {
-    int MaxDepth(TreeNode* root){
+    int check(TreeNode* root){
         if(root == nullptr) return 0;
-        return 1 + max(MaxDepth(root->left),MaxDepth(root->right));
+        int lefty = check(root->left);
+        if(lefty == -1) return -1;
+
+        int righty = check(root->right);
+        if(righty == -1) return -1;
+
+        if(abs(lefty - righty) > 1) return -1;
+        return max(lefty,righty) + 1;
     }
 public:
     bool isBalanced(TreeNode* root) {
         if(root == nullptr) return true;
-        if(!isBalanced(root->left)) return false;
-        if(!isBalanced(root->right)) return false;
-        return abs(MaxDepth(root->left) - MaxDepth(root->right)) > 1 ? false : true;
+        return check(root) != -1;
     }
 };
 
 int main(){
     Solution s;
+                        1
+                       / \
+                      2   2
+                     / \
+                    3
     TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
     root->right = new TreeNode(2);
     root->right->right = new TreeNode(3);
-    root->right->right->left = new TreeNode(4);
-    root->right->right->left->right = new TreeNode(4);
     cout<<s.isBalanced(root)<<endl;
     return 0;
 }
