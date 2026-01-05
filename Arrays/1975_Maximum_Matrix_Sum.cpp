64a94b5ce8e740e88331ec3140440b6a4b33f089
Anish17Bhat <bhatanish17@gmail.com>
2026-01-05 22:19:36 +0530

    update

--- Arrays/1975_Maximum_Matrix_Sum.cpp
+++ Arrays/1975_Maximum_Matrix_Sum.cpp
@@ -12,55 +12,21 @@ void printff(vector<vector<int>>&matrix, int n){
 }
 
 class Solution {
-    pair<int,int> findLowest(vector<vector<int>>&matrix,int i , int j, int n){
-        int lowest = INT_MAX;
-        pair<int,int>p;
-        int a;
-        int b;
-        if(i > 0 && matrix[i-1][j] <= lowest){
-            lowest = matrix[i-1][j];
-            a = i - 1;
-            b = j;
-        } 
-        if(i < n - 1 && matrix[i+1][j] <= lowest){
-            lowest = matrix[i+1][j];
-            a = i + 1;
-            b = j;
-        } 
-        if(j > 0 && matrix[i][j-1] <= lowest){
-            lowest = matrix[i][j-1];
-            a = i;
-            b = j - 1;
-        } 
-        if(j < n - 1 && matrix[i][j+1] <= lowest){
-            lowest = matrix[i][j+1];
-            a = i;
-            b = j + 1;
-        } 
-        p = make_pair(a,b);
-        return p;
-    }
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
+        long long sum = 0;
+        int count = 0;
+        int smallest = INT_MAX;
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++){
-                if(matrix[i][j] < 0){
-                    auto [a,b] = findLowest(matrix,i,j,n);
-                    if(matrix[a][b] > 0 && matrix[a][b] > matrix[i][j] && matrix[a][b] != abs(matrix[i][j])) continue;
-                    if(matrix[a][b] < 0 || matrix[a][b] <= abs(matrix[i][j])){
-                        matrix[i][j] *= -1;
-                        matrix[a][b] *= -1;
-                    }
-                }
+                if(matrix[i][j] < 0) count++;
+                sum += abs(matrix[i][j]);
+                smallest = min(abs(matrix[i][j]),smallest);
             }
         }
-        printff(matrix,n);
-        long long sum = 0;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                sum += matrix[i][j];
-            }
+        if(count % 2 == 1){
+            sum -= 2*smallest;
         }
         return sum;
     }
