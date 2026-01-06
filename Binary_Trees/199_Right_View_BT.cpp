dae00ee976e163df99883ecd2caeba86aa63d503
Anish17Bhat <bhatanish17@gmail.com>
2026-01-06 17:23:33 +0530

    update

---
+++ Binary_Trees/199_Right_View_BT.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        map<int,TreeNode*>mp;
+        int row = 0;
+        vector<int> result;     
+        if(root == nullptr) return result;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0 ; i < size ; i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                mp[row] = temp;
+                if(temp->left != nullptr) q.push(temp->left);
+                if(temp->right != nullptr) q.push(temp->right);
+            }
+            row++;
+        } 
+        for(auto ele : mp){
+            result.push_back(ele.second->val);
+        }
+        return result;
+    }
+};
+
+TreeNode* buildTree(const vector<optional<int>>& arr) {
+    if (arr.empty() || !arr[0].has_value())
+        return nullptr;
+
+    TreeNode* root = new TreeNode(arr[0].value());
+    queue<TreeNode*> q;
+    q.push(root);
+
+    int i = 1;
+
+    while (!q.empty() && i < arr.size()) {
+        TreeNode* curr = q.front();
+        q.pop();
+
+        // Left child
+        if (i < arr.size() && arr[i].has_value()) {
+            curr->left = new TreeNode(arr[i].value());
+            q.push(curr->left);
+        }
+        i++;
+
+        // Right child
+        if (i < arr.size() && arr[i].has_value()) {
+            curr->right = new TreeNode(arr[i].value());
+            q.push(curr->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main(){
+    Solution s;
+    vector<optional<int>> arr = {
+        1,2,3,nullopt,5,nullopt,4
+    };
+    TreeNode* root = buildTree(arr);
+    vector<int>result = s.rightSideView(root);
+    for(auto ele : result){
+        cout<<ele<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
