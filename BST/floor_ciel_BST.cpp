54e3ef3d51b6e806fab19a9a5100a0d2633a5cc1
Anish17Bhat <bhatanish17@gmail.com>
2026-01-27 22:59:54 +0530

    update

--- BST/floor_ciel_BST.cpp
+++ BST/floor_ciel_BST.cpp
@@ -1,67 +1,101 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNode {
+struct TreeNode
+{
     int val;
     TreeNode *left;
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
-
-class Solution{
-    int Floor = -1;
-    int Ceil = -1;
-    int findfloor(TreeNode* root, int key){
-        if(root == nullptr) return Floor;
-        if(root->val == key) return key;
-        else if(root->val < key){
-            Floor = max(Floor,root->val);
-            return findfloor(root->right,key);
-        } 
-        return findfloor(root->left,key);
-    }	
-    int findceil(TreeNode* root, int key){
-        if(root == nullptr) return Ceil;
-        if(root->val == key) return key;
-        else if(root->val > key){
-            if(Ceil == -1) Ceil = root->val;
-            else Ceil = min(Ceil,root->val);
-            return findceil(root->left,key);
-        } 
-        return findceil(root->right,key);
-    }	
-	public:
-		vector<int> floorCeilOfBST(TreeNode* root,int key){
-            return {findfloor(root,key),findceil(root,key)};
-		}
 };
 
-TreeNode* buildTree(const vector<optional<int>>& arr) {
+class Solution
+{
+    int Floor = -1;
+    int Ceil = -1;
+    int findfloor(TreeNode *root, int key)
+    {
+        // if(root == nullptr) return Floor;
+        // else if(root->val < key){
+        //     Floor = max(Floor,root->val);
+        //     return findfloor(root->right,key);
+        // }
+        // return findfloor(root->left,key);
+        while (root != nullptr)
+        {
+            if (root->val == key)
+                return key;
+            else if (root->val < key)
+            {
+                Floor = root->val;
+                root = root->right;
+            }
+            else
+                root = root->left;
+        }
+        return Floor;
+    }
+    int findceil(TreeNode *root, int key)
+    {
+        // if(root == nullptr) return Ceil;
+        // else if(root->val > key){
+        //     if(Ceil == -1) Ceil = root->val;
+        //     else Ceil = min(Ceil,root->val);
+        //     return findceil(root->left,key);
+        // }
+        // return findceil(root->right,key);
+        while (root != nullptr)
+        {
+            if (root->val == key)
+                return key;
+            else if (root->val > key)
+            {
+                Ceil = root->val;
+                root = root->left;
+            }
+            else
+                root = root->right;
+        }
+        return Ceil;
+    }
+
+public:
+    vector<int> floorCeilOfBST(TreeNode *root, int key)
+    {
+        return {findfloor(root, key), findceil(root, key)};
+    }
+};
+
+TreeNode *buildTree(const vector<optional<int>> &arr)
+{
     if (arr.empty() || !arr[0].has_value())
         return nullptr;
 
-    TreeNode* root = new TreeNode(arr[0].value());
-    queue<TreeNode*> q;
+    TreeNode *root = new TreeNode(arr[0].value());
+    queue<TreeNode *> q;
     q.push(root);
 
     int i = 1;
 
-    while (!q.empty() && i < arr.size()) {
-        TreeNode* curr = q.front();
+    while (!q.empty() && i < arr.size())
+    {
+        TreeNode *curr = q.front();
         q.pop();
 
         // Left child
-        if (i < arr.size() && arr[i].has_value()) {
+        if (i < arr.size() && arr[i].has_value())
+        {
             curr->left = new TreeNode(arr[i].value());
             q.push(curr->left);
         }
         i++;
 
         // Right child
-        if (i < arr.size() && arr[i].has_value()) {
+        if (i < arr.size() && arr[i].has_value())
+        {
             curr->right = new TreeNode(arr[i].value());
             q.push(curr->right);
         }
@@ -71,11 +105,12 @@ TreeNode* buildTree(const vector<optional<int>>& arr) {
     return root;
 }
 
-int main(){
+int main()
+{
     Solution s;
-    vector<optional<int>>arr = {8, 4, 12, 2, 6, 10, 14};
-    TreeNode* root = buildTree(arr);
-    vector<int>res = s.floorCeilOfBST(root,11);
-    cout<<res[0]<<" "<<res[1]<<endl;
+    vector<optional<int>> arr = {8, 4, 12, 2, 6, 10, 14};
+    TreeNode *root = buildTree(arr);
+    vector<int> res = s.floorCeilOfBST(root, 15);
+    cout << res[0] << " " << res[1] << endl;
     return 0;
 }
