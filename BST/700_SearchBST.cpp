f9f0946ce5b6c0f05eb5335e83ef4f5a22e85b1a
Anish17Bhat <bhatanish17@gmail.com>
2026-01-27 15:42:46 +0530

    update

---
+++ BST/700_SearchBST.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == nullptr) return nullptr;
+        if(root->val == val) return root;
+        else if(root->val > val) return searchBST(root->left,val);
+        return searchBST(root->right,val);
+    }
+};
+
+TreeNode* buildTree(const vector<optional<int>>& arr) {
+    if (arr.empty() || !arr[0].has_value())
+        return nullptr;
+
+    TreeNode* root = new TreeNode(arr[0].value());
+    queue<TreeNode*> q;
+    q.push(root);
+
+    int i = 1;
+
+    while (!q.empty() && i < arr.size()) {
+        TreeNode* curr = q.front();
+        q.pop();
+
+        // Left child
+        if (i < arr.size() && arr[i].has_value()) {
+            curr->left = new TreeNode(arr[i].value());
+            q.push(curr->left);
+        }
+        i++;
+
+        // Right child
+        if (i < arr.size() && arr[i].has_value()) {
+            curr->right = new TreeNode(arr[i].value());
+            q.push(curr->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main(){
+    Solution s;
+    vector<optional<int>>arr = {4,2,7,1,3};
+    TreeNode* root = buildTree(arr);
+    TreeNode* t = s.searchBST(root,3);
+    return t->val;
+    return 0;
+}
