06a0e61fe61462d179b0f1ec252bef77e2751e38
Anish17Bhat <bhatanish17@gmail.com>
2026-03-16 22:31:29 +0530

    update

--- Graphs/buildGraph.cpp
+++ Graphs/buildGraph.cpp
@@ -1,29 +1,41 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-        vector<vector<int>> builGraph(vector<vector<int>>&input){
-            int m = input[0][0];
-            vector<vector<int>>graph(m+1,vector<int>(m+1,0));
-            for(int i = 1 ; i < input.size() ; i++){
-                graph[input[i][0]][input[i][1]] = 1;
-                graph[input[i][1]][input[i][0]] = 1;
-            }
-            return graph;
+class Solution
+{
+public:
+    vector<vector<int>> builGraph(vector<vector<int>> &input)
+    {
+        int vtx = input[0][0];
+        int e = input[0][1];
+        vector<vector<int>> graph(vtx + 1);
+
+        for (int i = 1; i <= e; i++)
+        {
+            int u = input[i][0];
+            int v = input[i][1];
+
+            graph[u].push_back(v);
+            graph[v].push_back(u);
         }
+        return graph;
+    }
 };
 
-int main(){
+int main()
+{
     Solution s;
-    vector<vector<int>> input = {{5,6},{1,2},{1,3},{2,4},{3,4},{3,5},{4,5}};
-    vector<vector<int>>result = s.builGraph(input);
-    for(int i = 1 ; i < result.size() ; i++){
-        for(int j = 1 ; j < result[0].size() ; j++){
-            cout<<result[i][j]<<" ";
+    vector<vector<int>> input = {{5, 6}, {1, 2}, {1, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 5}};
+    vector<vector<int>> result = s.builGraph(input);
+    for (int i = 1; i < result.size(); i++)
+    {
+        cout << i << " -> ";
+        for (int j : result[i])
+        {
+            cout << j << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
