795b9809ccbdde3e120968de010e4919ab640f0e
Anish17Bhat <bhatanish17@gmail.com>
2026-03-20 16:40:49 +0530

    update

--- Graphs/traversal_graphs.cpp
+++ Graphs/traversal_graphs.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 class Solution {
-    void buildGraph(int n, vector<int>adj[],vector<vector<int>>&adjacency){
-        int m = sizeof(adj)/sizeof(adj[0]);
+    void buildGraph(int n, int m, vector<int>adj[],vector<vector<int>>&adjacency){
         for(int i = 0 ; i < m ; i++){
             int vertex1 = adj[i][0];
             int vertex2 = adj[i][1];
@@ -13,16 +12,17 @@ class Solution {
         }
     }
   public:
-    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+    vector<int> dfsOfGraph(int V, int m, vector<int> adj[]) {
         vector<int>res;
         vector<vector<int>>adjacency(V);
-        buildGraph(V,adj,adjacency);
+        buildGraph(V,m,adj,adjacency);
         vector<int>visit(V,0);
         for(int i = 0 ; i < V ; i++){
             if(!visit[i]){
-                dfs(V,visit,adjacency,res);
+                dfs(i,visit,adjacency,res);
             }
         }
+        return res;
     }
 
     void dfs(int node , vector<int>&visit, vector<vector<int>>&adjacency, vector<int>&res){
@@ -35,14 +35,51 @@ class Solution {
         }
     }
     
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-        
+    vector<int> bfsOfGraph(int V, int m,vector<int> adj[]) {
+        vector<int>res;
+        vector<vector<int>>adjacency(V);
+        buildGraph(V,m,adj,adjacency);
+        vector<int>visit(V,0);
+        queue<int>q;
+        for(int i = 0 ; i < V ; i++){
+            if(!visit[i]){
+                q.push(i);
+                visit[i] = 1;
+                res.push_back(i);
+                while(!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    for(int ele : adjacency[node]){
+                        if(!visit[node]){
+                            visit[ele] = 1;
+                            res.push_back(ele);
+                        }
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
 
 
 
 int main(){
-
+    Solution s;
+    int m = 4;
+    vector<int> input[] = {{0,1},{0,2},{0,3},{2,4}};
+    int V = 5;
+    vector<int>dfs = s.dfsOfGraph(V,m,input);
+    cout<<"DFS: ";
+    for(int ele : dfs){
+        cout<<ele<<" ";
+    }
+    vector<int>bfs = s.bfsOfGraph(V,m,input);
+    cout<<endl;
+    cout<<"BFS: ";
+    for(int ele : bfs){
+        cout<<ele<<" ";
+    }
+    cout<<endl;
     return 0;
 }
