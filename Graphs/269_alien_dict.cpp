c99a043fe41e06ecb517bb1bd9c0429512867fcf
Anish17Bhat <bhatanish17@gmail.com>
2026-06-03 23:38:37 +0530

    update

--- Graphs/269_alien_dict.cpp
+++ Graphs/269_alien_dict.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class Solution {
-    vector<vector<int>> buildGraph(string dict[], int N, int K, unordered_map<char,int>&mp, vector<int>&indegree){
+    vector<vector<int>> buildGraph(string dict[], int N, int K,vector<int>&indegree){
         vector<vector<int>> adj(K);
         for(int i = 0 ; i < N-1 ; i++){
             string string1 = dict[i];
@@ -12,8 +12,8 @@ class Solution {
             int n1 = string1.size(), n2 = string2.size();
             while(l1 < n1 && l2 < n2){
                 if(string1[l1] != string2[l2]){
-                    adj[mp[string1[l1]]].push_back(mp[string2[l2]]);
-                    indegree[mp[string2[l2]]]++;
+                    adj[string1[l1] - 'a'].push_back(string2[l2] - 'a');
+                    indegree[string2[l2] - 'a']++;
                     break;
                 }
                 l1++;
@@ -24,16 +24,8 @@ class Solution {
     }
 public:
 	string findOrder(string dict[], int N, int K) {
-        unordered_map<char,int> mp1;
-        unordered_map<int,char> mp2;
-        int num = 0;
-        for(char c = 'a' ; c < 'a' + K ; c++){
-            mp1[c] = num;
-            mp2[num] = c;
-            num++;
-        }
         vector<int>indegree(K,0);
-        vector<vector<int>> adj = buildGraph(dict,N,K,mp1,indegree);
+        vector<vector<int>> adj = buildGraph(dict,N,K,indegree);
         queue<int>q;
         for(int i = 0 ; i < K ; i++){
             if(indegree[i] == 0) q.push(i);
@@ -42,12 +34,13 @@ public:
         while(!q.empty()){
             int v = q.front();
             q.pop();
-            ans += mp2[v];
+            ans += 'a' + v;
             for(int e : adj[v]){
                 indegree[e]--;
                 if(indegree[e] == 0) q.push(e);
             }
         }
+        for(int i : indegree) if(i != 0) return "";
         return ans;
 	}
 };
@@ -55,7 +48,8 @@ public:
 
 int main(){
     Solution s;
-    string dict[5] = {"baa","abcd","abca","cab","cad"};
-    cout<<s.findOrder(dict,5,4)<<endl;
+    // string dict[5] = {"baa","abcd","abca","cab","cad"};                       // valid testcase   return bdac
+    string dict[7] = {"baa","bad","bac","abcd","abca","cab","cad"};             // invalid testcase  return ""
+    cout<<s.findOrder(dict,7,4)<<endl;
     return 0;
 }
