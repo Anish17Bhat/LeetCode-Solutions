c1389dbbeaf13356ed1fb767911f6e25fb0451ec
Anish17Bhat <bhatanish17@gmail.com>
2026-06-21 10:05:28 +0530

    update

---
+++ Graphs/bellman_ford_algo.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+	vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        vector<int>distance(V,1e9);
+        distance[S] = 0;
+        int n = edges.size();
+        int i;
+        for(i = 1 ; i <= V - 1 ; i++){
+            for(int j = 0 ; j < n ; j++){
+                int u = edges[j][0];
+                int v = edges[j][1];
+                int wt = edges[j][2];
+                if(distance[u] != 1e9 && distance[u] + wt < distance[v]){
+                    distance[v] = distance[u] + wt;
+                }
+            }
+        }
+
+        for(int j = 0 ; j < n ; j++){
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int wt = edges[j][2];
+            if(distance[u] != 1e9 && distance[u] + wt < distance[v]){
+                distance[v] = distance[u] + wt;
+                return {-1};
+            }
+        }
+
+        return distance;
+	}
+};
+
+
+int main(){
+    vector<vector<int>> edges = {{3, 2, 6}, {5, 3, 1}, {0, 1, 5}, {1, 5, -3}, {1, 2, -2}, {3, 4, -2}, {2, 4, 3}};
+    return 0;
+}
